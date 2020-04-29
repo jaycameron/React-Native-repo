997d31aac1cbe658c8e8575a1a74c47113ee4f42
Jordan Cameron <jrobertcameron@gmail.com>
2020-04-29 09:30:28 -0700

    RN Icons

--- Components/AboutComponent.js
+++ Components/AboutComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { ScrollView, View, Text, FlatList } from 'react-native';
-import Card from 'react-navigation/src/views/StackView/StackViewCard';
 import { PARTNERS } from '../shared/partners';
 import { ListItem } from 'react-native-elements';
+import { Card } from 'react-native-elements';
+
 
 class About extends Component {
   constructor(props) {
@@ -24,29 +25,29 @@ class About extends Component {
       <ScrollView>
         <Mission />
         <Card title="Community Partners">
-          <FlatList></FlatList>
+          <FlatList
+          data={this.state.partners}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={renderPartner}
+            />
         </Card>
       </ScrollView>
     );
   }
 }
 
-function Mission({ item }) {
-  if (item) {
-    return (
-      <Card title="Our Mission">
-        <Text>
+function Mission(){
+  return (<Card title="Our Mission">
+          <Text>
           We present a curated database of the best campsites in the vast woods
-          and backcountry of the World Wide Web Wilderness. We increase access
+            and backcountry of the World Wide Web Wilderness. We increase access
           to adventure for the public while promoting safe and respectful use of
           resources. The expert wilderness trekkers on our staff personally
           verify each campsite to make sure that they are up to our standards.
           We also present a platform for campers to share reviews on campsites
-          they have visited with each other.
-        </Text>
-      </Card>
-    );
-  }
+         they have visited with each other.
+       </Text>
+      </Card>)
 }
 
 export default About;
